555679449e823aa3b5c77ff7ee31b940b9429769
Hackbright Student <no-reply@hackbrightacademy.com>
2019-10-11 16:15:28 -0700

    Created instance method for InternationalMelonOrder class - get_additional_fee

--- melons.py
+++ melons.py
@@ -11,8 +11,12 @@ class AbstractMelonOrder():
 
     def get_total(self):
         """Calculate price, including tax."""
-        base_price = 5
-        total = (1 + self.tax) * self.qty * base_price
+        if self.species == "Christmas melon":
+            base_price = 7.5
+        else:
+            base_price = 5
+        
+        total = (1 + self.tax) * self.qty * base_price 
 
         return total
 
@@ -36,9 +40,20 @@ class InternationalMelonOrder(AbstractMelonOrder):
     order_type = "international"
     tax = 0.17
 
+
     def __init__(self, species, qty, country_code):
         super().__init__(species, qty)
         self.country_code = country_code
+
+
+    def get_additional_fee(self, qty):
+        
+        if qty < 10: 
+            flat_fee = 3
+        else:
+            flat_fee = 0
+
+        return flat_fee
 
 
     def get_country_code(self):
