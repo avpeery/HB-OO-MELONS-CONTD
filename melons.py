44cf247afcced114f8712fa33132c4020d8b3136
Hackbright Student <no-reply@hackbrightacademy.com>
2019-10-11 16:54:34 -0700

    OO Melons Part 3 Completed

--- melons.py
+++ melons.py
@@ -19,7 +19,7 @@ class AbstractMelonOrder():
         total = (1 + self.tax) * self.qty * base_price 
 
         if type(self) == "InternationalMelonOrder" and self.qty < 10:
-            total = total +3
+            total = total + 3
 
         return total
 
@@ -53,3 +53,16 @@ class InternationalMelonOrder(AbstractMelonOrder):
         """Return the country code."""
 
         return self.country_code
+
+class GovernmentMelonOrder(AbstractMelonOrder):
+    tax = 0
+    order_type = "government"
+
+    def __init__(self, species, qty, passed_inspection = False):
+        super().__init__(species, qty)
+        self.passed_inspection = passed_inspection
+
+    def marked_inspection(self):
+        if self.passed_inspection == False:
+            self.passed_inspection = True
+        return self.passed_inspection
